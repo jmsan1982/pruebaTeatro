9587c7f40baefba822b5bf0776b148dbec6d01c3
jmsan1982 <jmsanchez1982@gmail.com>
2021-12-15 18:16:25 +0100

    agregada columna apellidos a users y creada migracion

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        //esta es la home
         return view('home');
     }
 }
--- config/database.php
+++ config/database.php
@@ -45,6 +45,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
+            'engine' => 'InnoDB',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
